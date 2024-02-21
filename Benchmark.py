69f71da0f44f2dd69acf7b330f88715eff321869
QuantumJoel <joel.neumann@onlinehome.de>
2024-02-21 20:48:43 +0100

    Updated the code, add plot of the execution time

--- Benchmark.py
+++ Benchmark.py
@@ -11,31 +11,41 @@ def numbers_generater(N):
     file_path = 'data/numbers.txt'
     with open(file_path, 'w') as file:
         file.write(str(numbers))
-numbers_generater(100)
-
+numbers_generater(10_000)
 
 def run_code(file_path):
     local_start_time = time.time() 
     subprocess.run(['python', file_path], check=True)
     local_end_time = time.time()
-    return file_path, local_end_time - local_start_time
+    return os.path.basename(file_path), local_end_time - local_start_time
 
 def main():
     files = ['numpy_sort.py', 'Quicksort.py', 'Selectionsort.py', 'Bubblesort.py']
     base_path = './src/python/'
-    start_time = time.time()
-    result_time = []
+    file_names = []
+    execution_times = []
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures = [executor.submit(run_code, base_path + file) for file in files]
         for future in concurrent.futures.as_completed(futures):
-            files, execution_time = future.result()
-            print(f"{files} execution time: {execution_time} seconds")
-            result_time.append([files, execution_time])
-    print(result_time)
+            file_name, execution_time = future.result()
+            file_names.append(file_name)
+            execution_times.append(execution_time)
 
-    end_time = time.time()
-    print(f"Total execution time: {end_time - start_time} seconds")
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(file_names, execution_times, color='skyblue')
+    plt.xlabel('Dateiname')
+    plt.ylabel('Ausführungszeit (s)')
+    plt.title('Ausführungszeit der Sortieralgorithmen')
+    plt.xticks(rotation=45)
+
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 4), ha='center', va='bottom')
+
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
     main()
+
