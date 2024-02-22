09bb764df75437d0b266245dca8e9ec923b4ff25
lean-he <leanhemmers@gmail.com>
2024-02-22 15:53:35 +0100

    Change Quicksort to be static and add multithreading sorting

--- Benchmark/src/main/java/Main.java
+++ Benchmark/src/main/java/Main.java
@@ -2,32 +2,35 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("### Quicksort ###");
-    int[] numbers = generateNumbers(20_000_000);
+    int size = 50_000_000;
+    if(args.length > 0) {
+      size = Integer.parseInt(args[0]);
+    }
+    int[] numbers = generateNumbers(size);
     float resultTime1 = runBenchmarkSingleThread(numbers);
-    System.out.printf(Locale.ENGLISH, "Sorting time: %.3f seconds (single)", resultTime1);
-    //TODO
-//    float resultTime2 = runBenchmarkMultiThread(numbers);
-//    System.out.printf(Locale.ENGLISH, "Sorting time: %.3f seconds (multi)", resultTime2);
+    float resultTime2 = runBenchmarkMultiThread(numbers);
+
+    System.out.printf(Locale.ENGLISH, "Quicksort with %,d elements%n" +
+        "Sorting time: %.3f seconds (single)%n" +
+        "Sorting time: %.3f seconds (multi)%n",
+        size, resultTime1, resultTime2);
   }
 
   public static float runBenchmarkSingleThread(int[] numbers) {
     long startTime = System.currentTimeMillis();
-    Quicksort quicksort = new Quicksort();
-    quicksort.sort(numbers);
+    Quicksort.sort(numbers);
     long endTime = System.currentTimeMillis();
     // Convert to seconds by dividing with 1000
     return (endTime - startTime) / 1000f;
   }
 
-//  public static float runBenchmarkMultiThread(int[] numbers) {
-//    long startTime = System.currentTimeMillis();
-//    QuicksortMulti quicksort = new QuicksortMulti();
-//    quicksort.sort(numbers);
-//    long endTime = System.currentTimeMillis();
-//    // Convert to seconds by dividing with 1000
-//    return (endTime - startTime) / 1000f;
-//  }
+  public static float runBenchmarkMultiThread(int[] numbers) {
+    long startTime = System.currentTimeMillis();
+    QuicksortMulti.sort(numbers);
+    long endTime = System.currentTimeMillis();
+    // Convert to seconds by dividing with 1000
+    return (endTime - startTime) / 1000f;
+  }
 
   public static int[] generateNumbers(int size) {
     int[] array = new int[size];
--- Benchmark/src/main/java/Quicksort.java
+++ Benchmark/src/main/java/Quicksort.java
@@ -1,30 +1,35 @@
+import java.util.Arrays;
+
 public class Quicksort {
 
-    public void sort(int[] numbers) {
-        quicksort(0, numbers.length-1, numbers);
-    }
+  private int l, r;
+  private int[] numbers;
 
-    private int divide(int l, int r, int[] numbers) {
-        int p = numbers[(l + r) / 2];
-        while (true) {
-            while (numbers[l] < p)
-                l++;
-            while (numbers[r] > p)
-                r--;
-            if (l < r) {
-                int tmp = numbers[l];
-                numbers[l] = numbers[r];
-                numbers[r] = tmp;
-            } else
-                return r;
-        }
-    }
+  public static void sort(int[] numbers) {
+    quicksort(0, numbers.length-1, numbers);
+  }
 
-    private void quicksort(int l, int r, int[] numbers) {
+  public static int divide(int l, int r, int[] numbers) {
+    int p = numbers[(l + r) / 2];
+      while (true) {
+        while (numbers[l] < p)
+          l++;
+        while (numbers[r] > p)
+          r--;
         if (l < r) {
-            int d = divide(l, r, numbers);
-            quicksort(l, d, numbers);
-            quicksort(d+1, r, numbers);
-        }
+          int tmp = numbers[l];
+          numbers[l] = numbers[r];
+          numbers[r] = tmp;
+        } else
+          return r;
+      }
+  }
+
+  public static void quicksort(int l, int r, int[] numbers) {
+    if (l < r) {
+      int d = divide(l, r, numbers);
+      quicksort(l, d, numbers);
+      quicksort(d+1, r, numbers);
     }
+  }
 }
